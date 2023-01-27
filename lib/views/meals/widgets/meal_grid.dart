ace5c129a09c556b4cebc441e0eea7049c4e64cb
Kounex <r.schramowski@googlemail.com>
2023-01-27 22:00:05 +0100

    wirst usage of the pods

--- lib/views/meals/widgets/meal_grid.dart
+++ lib/views/meals/widgets/meal_grid.dart
@@ -1,46 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:uuid/uuid.dart';
+import 'package:mealo/stores/shared/meal.dart';
 
 import '../../../types/api/meal.dart';
 import 'meal_card.dart';
 
 const double kMealGridCrossAxisExtent = 200.0;
 
-class MealGrid extends StatelessWidget {
+class MealGrid extends ConsumerWidget {
   const MealGrid({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<List<Meal>> asyncMeals = ref.watch(mealsProvider);
+
     final int columnCount =
         MediaQuery.of(context).size.width ~/ kMealGridCrossAxisExtent;
 
-    return AnimationLimiter(
-      child: SliverGrid.builder(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200.0,
-          mainAxisExtent: 150.0,
-          mainAxisSpacing: 24.0,
-          crossAxisSpacing: 24.0,
-        ),
-        itemCount: 10,
-        itemBuilder: (context, index) => AnimationConfiguration.staggeredGrid(
-          position: index,
-          duration: const Duration(milliseconds: 375),
-          columnCount: columnCount,
-          child: ScaleAnimation(
-            child: FadeInAnimation(
-              child: MealCard(
-                meal: Meal(
-                  uuid: const Uuid().v4(),
-                  createdAt: DateTime.now(),
-                  name: 'Penne Gorgonzola mit Baguette',
-                ),
-              ),
-            ),
-          ),
+    return asyncMeals.when(
+      loading: () => const SliverToBoxAdapter(
+        child: Center(
+          child: RefreshProgressIndicator(),
         ),
       ),
+      error: (error, stackTrace) => const Center(
+        child: SliverToBoxAdapter(
+          child: Text('ERROR'),
+        ),
+      ),
+      data: (meals) => meals.isNotEmpty
+          ? AnimationLimiter(
+              child: SliverGrid.builder(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200.0,
+                  mainAxisExtent: 150.0,
+                  mainAxisSpacing: 24.0,
+                  crossAxisSpacing: 24.0,
+                ),
+                itemCount: meals.length,
+                itemBuilder: (context, index) =>
+                    AnimationConfiguration.staggeredGrid(
+                  position: index,
+                  duration: const Duration(milliseconds: 375),
+                  columnCount: columnCount,
+                  child: ScaleAnimation(
+                    child: FadeInAnimation(
+                      child: MealCard(
+                        meal: meals[index],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : const SliverToBoxAdapter(
+              child: Text('No meals yet! ò.ó'),
+            ),
     );
   }
 }
